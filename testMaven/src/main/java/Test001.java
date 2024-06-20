c64607cb309133b46ca0502141614df867f692c3
dong <1047468419@qq.com>
2024-06-20 17:56:36 +0800

    测试

---
+++ testMaven/src/main/java/Test001.java
@@ -0,0 +1,8 @@
+import com.alibaba.fastjson.JSONObject;
+
+public class Test001 {
+    public static void main(String[] args) {
+        JSONObject object = new JSONObject();
+        System.out.println("object = " + object);
+    }
+}
